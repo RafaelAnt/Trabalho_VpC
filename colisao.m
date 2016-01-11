a485a235af2dda1047000e2f290931c45f2b3be3
Rafael Antunes <rafael.uminho@hotmail.com>
2016-01-11 16:33:22 +0000

    implementação de algoritmo de deteção de objetos

--- colisao.m
+++ colisao.m
@@ -1,5 +1,5 @@
-clear
-
+clear;
+%{ Nosso Código, Não é usado...
 v = VideoReader('vids/car.avi');
 
 while hasFrame(v)
@@ -13,11 +13,33 @@ frame = readFrame(v);
 %bw=im2bw(frame,avgInten/255);
 %bw=im2bw(frame,35/255);
 
-
 imshow(frame)
 
 end
+%}
 
+% Create System objects used for reading video, detecting moving objects,
+% and displaying the results.
+obj = setupObjects();
+
+tracks = initializeTracks(); % Create an empty array of tracks.
+
+nextId = 1; % ID of the next track
+
+% Detect moving objects, and track them across video frames.
+while ~isDone(obj.reader)
+    frame = obj.reader.step();                                              %Read a new frame.
+    [centroids, bboxes, mask] = detectObjects(obj,frame);
+    predictNewLocationsOfTracks(tracks);
+    [assignments, unassignedTracks, unassignedDetections] = detectionToTrackAssignment(tracks,centroids);
+
+    updateAssignedTracks(assignments);
+    updateUnassignedTracks(unassignedTracks);
+    deleteLostTracks(tracks);
+    nextId = createNewTracks(unassignedDetections,centroids,bboxes,nextId,tracks);
+
+    displayTrackingResults(frame,mask,tracks,obj);
+end
 
 
 %{
