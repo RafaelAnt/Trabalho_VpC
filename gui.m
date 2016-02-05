ae2e7941d346229c2bc01d4552128957a452acb0
Bruno Barbosa <brunobarbosa.caos@gmail.com>
2016-02-05 23:11:39 +0000

    updated report

--- gui.m
+++ gui.m
@@ -22,7 +22,7 @@ function varargout = gui(varargin)
 
 % Edit the above text to modify the response to help gui
 
-% Last Modified by GUIDE v2.5 05-Feb-2016 21:44:52
+% Last Modified by GUIDE v2.5 05-Feb-2016 23:02:38
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -149,3 +149,10 @@ function video_text_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+
+% --------------------------------------------------------------------
+function Untitled_1_Callback(hObject, eventdata, handles)
+% hObject    handle to Untitled_1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
